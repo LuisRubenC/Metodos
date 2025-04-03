cb6c2e488b78da35232b8f184eb719e530fea9dd
whoismiranda2 <andreamirandapg@gmail.com>
2025-04-03 01:09:58 -0600

    JuegoAdivinanza.java
    Se añadó lo necesario para la función main y se hizo el método para  verificar si el número ingresado por el usuario coincide con el número aleatorio.

--- JuegoAdivinanza.java
+++ JuegoAdivinanza.java
@@ -15,8 +15,38 @@ public class JuegoAdivinanza {
         System.out.println("¡Bienvenido al juego de adivinanza!");
         System.out.println("Debes adivinar un número entre 1 y 100.");
         System.out.println("Tienes 10 intentos. ¡Buena suerte!");
+        
+        while (intentos < maxIntentos) {
+            System.out.println("Intento " + (intentos + 1) + " de " + maxIntentos);
+            int numeroUsuario = obtenerEntrada(scanner);
 
+            intentos++; 
+
+            if (verificarNumero(numeroUsuario, numeroAleatorio)) {
+                System.out.println("Has adivinado el número en " + intentos + " intentos.");
+                adivinado = true;
+                break;
+            }
+        }
+
+        if (!adivinado) {
+            System.out.println("Lo siento, has agotado tus intentos. El número era " + numeroAleatorio);
+        }
+
+        scanner.close();
+    }
 
 
  }
+    public static boolean verificarNumero(int numeroUsuario, int numeroAleatorio) {
+        if (numeroUsuario == numeroAleatorio) {
+            System.out.println("¡Felicidades! Has adivinado el número.");
+            return true;
+        } else if (numeroUsuario < numeroAleatorio) {
+            System.out.println("El número es mayor. Intenta de nuevo.");
+        } else {
+            System.out.println("El número es menor. Intenta de nuevo.");
+        }
+        return false;
+    }
 }
